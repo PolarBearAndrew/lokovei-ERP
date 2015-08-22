9c0d7a1c7f51342a882bd0d8d0e3e219fdafd144
Andrew <chenpoanandrew@gmail.com>
2015-08-22 23:14:54 +0800

    crud add btn

---
+++ lokovei-ERP/asset/javascript/crud.js
@@ -0,0 +1,164 @@
+$(document).ready(function() {
+
+  // edit tables=======================
+
+  var auth = ['員工', '主管']
+
+  $('html, body').on('click', '.edit', function(){
+
+    // icon edit/save
+    var icoEdit = '<span aria-hidden="true" class="glyphicon glyphicon-pencil"></span>';
+    var icoSave = '<span aria-hidden="true" class="glyphicon glyphicon-floppy-open"></span>';
+
+    // get info we need
+    var id = $(this).attr('data-orderId');
+    var onEdit = parseInt( $(this).attr('data-onEdit') );
+    var arr = $('[data-orderId="' + id + '"] td[data-ctrl]');
+
+    // going to save
+    if( onEdit === 1 ){
+      // chage btn icon
+      $(this).empty().append(icoEdit);
+
+      // loop to read each <td>
+      for (var i = 0; i < arr.length; i++) {
+        var show = controlsValue( $(arr[i]).attr('data-ctrl'), arr[i] );
+        if(show) $(arr[i]).empty().append( show );
+      };
+
+      //  !!!
+      // need ajax save api
+      //  !!!
+
+    // going to edit
+    }else{
+      // change btn icon
+      $(this).empty().append(icoSave);
+
+      // loop to read each <td>
+      for (var i = 0; i < arr.length; i++) {
+        var ctrl = controls($(arr[i]).attr('data-ctrl'), $(arr[i]).html());
+        if(ctrl) $(arr[i]).empty().append( ctrl );
+      };
+
+      //init date picker
+      // initDatePicker();
+    }
+
+    //  true false switch
+    onEdit -= 1
+    onEdit = onEdit * onEdit;
+    $(this).attr('data-onEdit', onEdit);
+
+    return false;
+  });
+
+  function controls( ctrl, value ){
+
+    var show;
+
+    value = value.replace(/ /g, '');
+
+    switch(ctrl){
+
+      case 'none':
+        show = value;
+        break;
+
+      case 'auth':
+        show = buildSelector( auth, value )
+        break;
+
+      case 'text':
+        show = '<input class="form-control" type="text" value="' + value + '"></input>'
+        break;
+    }
+    return show;
+  }
+
+  function controlsValue( ctrl, obj ){
+
+    var show;
+
+    switch(ctrl){
+
+      case 'none':
+        show =  $(obj).text();
+        break;
+
+      case 'auth':
+        show = $(obj).children('select').val() || ' ';
+        break;
+
+      case 'text':
+        //console.log('show', show);
+        show = $(obj).children('input').val() || ' ';
+        break;
+    }
+    return show;
+  }
+
+  //build <select>
+  function buildSelector( arr, selected ){
+
+    var select = '<select class="form-control">';
+    var option = '<option value="@val">@val</option>';
+    var optionChecked = '<option value="@val" selected>@val</option>';
+    var selectEnd = '</select>';
+    var tmp = '';
+
+    for (var i = 0; i < arr.length; i++) {
+      if( arr[i] !== selected )
+        tmp += option.replace( /@val/g, arr[i] );
+      else
+        tmp += optionChecked.replace( /@val/g, arr[i] );
+
+    };
+
+    return select + tmp + selectEnd;
+  }
+
+  // end edit table
+  // ===========================
+  //
+
+  $('html, body').on('click', '#add', function(){
+
+    var id = $('table#main tbody tr').length + 1;
+    var arr = $('table#main tbody tr:first-child td');
+    var row = '<tr data-orderid="' + id +'">';
+
+
+
+    for (var i = 0; i < arr.length ; i++) {
+
+      if( i == arr.length - 1){
+
+        row += '<td><button class="btn btn-danger ctrl-table"><span aria-hidden="true" class="glyphicon glyphicon-trash"></span></button><button data-orderid="@id" data-onedit="0" class="btn btn-warning ctrl-table edit"><span aria-hidden="true" class="glyphicon glyphicon-pencil"></span></button></td></tr>'.replace(/@id/g, id);
+        break;
+      }
+
+      var td = '<td @attr></td>'
+      var ctrl = $(arr[i]).attr('data-ctrl');
+
+      if(ctrl) td = td.replace(/@attr/g, 'data-ctrl="' + ctrl + '"')
+      else td = td.replace(/@attr/g,'')
+
+      row += td;
+    };
+
+    $('table#main tbody').append(row);
+    return false
+  });
+
+});//  end doc reade
+
+
+
+
+
+
+
+
+
+
--- lokovei-ERP/routes/index.js
+++ lokovei-ERP/routes/index.js
@@ -14,4 +14,25 @@ router.get('/factory', function(req, res, next) {
   res.render('queue_factory');
 });
 
+router.get('/crud/:part', function(req, res, next) {
+
+  let schema = [
+    { title: '使用者姓名', ctrl: 'text' },
+    { title: '帳號', ctrl: 'text' },
+    { title: '密碼', ctrl: 'text' },
+    { title: '權限等級', ctrl: 'auth' } //authorization
+  ];
+
+  let data = [
+    [ '陳柏安', 'Andrew', '123', '員工'],
+    [ '蔡政欽', 'Srt', '123', '員工'],
+    [ '洪于雅', 'Doro', '123', '員工'],
+    [ '雷尚樺', 'Ray', '123', '員工'],
+    [ '陳思璇', 'Hsuan', '123', '員工'],
+  ];
+
+  res.render('crud', { schema: schema, data: data });
+});
+
+
 module.exports = router;
