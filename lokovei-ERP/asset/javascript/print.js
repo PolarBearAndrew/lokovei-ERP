6e80fb617e2ad840df77eb98cf88e9fe885a2d55
Andrew <chenpoanandrew@gmail.com>
2015-09-10 17:24:11 +0800

    print end

---
+++ lokovei-ERP/asset/javascript/print.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+
+//   $('html, body').on('onchange', 'input', function(){
+//     var inputs = $('input');
+//     for( var i = 0; i < inputs.length; i++){
+//       inputs[i]
+//     }
+//   });
+
+// })
